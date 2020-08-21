bbe4a74b71f224d27c1548f8951f5ae75ccbb97f
Judith Pillado <judith.pillado@gmail.com>
2020-08-21 00:19:09 -0500

    Add Pet seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,8 @@
 alive = Shelter.create!(name: "ALIVE Rescue", address: "1214 W Monroe St", city: "Chicago", state: "IL", zip: 60607)
 harmony = Shelter.create!(name: "Harmony House for Cats", address: "2914 N Elston Ave", city: "Chicago", state: "IL", zip: 60607)
 paws = Shelter.create!(name: "PAWS Chicago", address: "1997 N Clybourn Ave", city: "Chicago", state: "IL", zip: 60618)
+
+# Pets:
+diabla = alive.pets.create!(name: "Diabla", age: 3, sex: "Female", image: "https://files.slack.com/files-pri/T029P2S9M-F0174DQK1M5/20200630_120130.jpg", current_shelter: "ALIVE Rescue")
+prieto = harmony.pets.create!(name: "Prieto", age: 3, sex: "Male", image: "https://www.thesprucepets.com/thmb/-UHyBMUvHaMyFuCPWRnoYgLhVgA=/2304x1296/smart/filters:no_upscale()/close-up-of-cat-lying-on-floor-at-home-908763830-1d61bee6961b45ee8a55bdfa5da1ebb3.jpg", current_shelter: "Harmony House for Cats")
+gordo = paws.pets.create!(name: "Gordo", age: 5, sex: "Male", image: "https://www.treehugger.com/thmb/xG5q0YBTTGuLVIVpiCD8eohv3AE=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__mnn__images__2017__04__lop-earred-rabbit-d47323a1ab3847398463b3d6a1d7119b.jpg", current_shelter: "PAWS Chicago")
