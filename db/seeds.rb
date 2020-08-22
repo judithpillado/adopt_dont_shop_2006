cf5e4b7b29223106d30ed32b884083a26128d4fd
Judith Pillado <judith.pillado@gmail.com>
2020-08-21 23:33:06 -0500

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,6 @@ harmony = Shelter.create!(name: "Harmony House for Cats", address: "2914 N Elsto
 paws = Shelter.create!(name: "PAWS Chicago", address: "1997 N Clybourn Ave", city: "Chicago", state: "IL", zip: 60618)
 
 # Pets:
-diabla = alive.pets.create!(name: "Diabla", age: 3, sex: "Female", image: "https://files.slack.com/files-pri/T029P2S9M-F0174DQK1M5/20200630_120130.jpg", current_shelter: "ALIVE Rescue")
-prieto = harmony.pets.create!(name: "Prieto", age: 3, sex: "Male", image: "https://www.thesprucepets.com/thmb/-UHyBMUvHaMyFuCPWRnoYgLhVgA=/2304x1296/smart/filters:no_upscale()/close-up-of-cat-lying-on-floor-at-home-908763830-1d61bee6961b45ee8a55bdfa5da1ebb3.jpg", current_shelter: "Harmony House for Cats")
-gordo = paws.pets.create!(name: "Gordo", age: 5, sex: "Male", image: "https://www.treehugger.com/thmb/xG5q0YBTTGuLVIVpiCD8eohv3AE=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__mnn__images__2017__04__lop-earred-rabbit-d47323a1ab3847398463b3d6a1d7119b.jpg", current_shelter: "PAWS Chicago")
+diabla = alive.pets.create!(name: "Diabla", image: "https://bauerfrodoprod-cdn.azureedge.net/uploads/e3bb5459-077e-4633-a793-c6056dacb89e-ls4618_42_gi_172600883.jpg", age: 3, sex: "Female",  current_shelter: "ALIVE Rescue")
+prieto = harmony.pets.create!(name: "Prieto", image: "https://www.thesprucepets.com/thmb/-UHyBMUvHaMyFuCPWRnoYgLhVgA=/2304x1296/smart/filters:no_upscale()/close-up-of-cat-lying-on-floor-at-home-908763830-1d61bee6961b45ee8a55bdfa5da1ebb3.jpg", age: 3, sex: "Male",  current_shelter: "Harmony House for Cats")
+gordo = paws.pets.create!(name: "Gordo", image: "https://www.treehugger.com/thmb/xG5q0YBTTGuLVIVpiCD8eohv3AE=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__mnn__images__2017__04__lop-earred-rabbit-d47323a1ab3847398463b3d6a1d7119b.jpg", age: 5, sex: "Male",  current_shelter: "PAWS Chicago")
