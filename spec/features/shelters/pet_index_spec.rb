0e0ca5c9b03d9aebb25fff649be2150592cbb8a6
Judith Pillado <judith.pillado@gmail.com>
2020-08-22 18:58:10 -0500

    Delete user story info

--- spec/features/shelters/pet_index_spec.rb
+++ spec/features/shelters/pet_index_spec.rb
@@ -31,16 +31,3 @@ RSpec.describe "Shelter Pet Index Page" do
   end
 
 end
-
-
-
-
-# User Story 8, Shelter Pets Index
-#
-# As a visitor
-# When I visit '/shelters/:shelter_id/pets'
-# Then I see each Pet that can be adopted from that Shelter with that shelter_id including the Pet's:
-# - image
-# - name
-# - approximate age
-# - sex
