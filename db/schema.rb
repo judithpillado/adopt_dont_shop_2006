44d42fec226fc71cbb9c8be9d44238c8530823a8
Judith Pillado <judith.pillado@gmail.com>
2020-08-22 20:01:54 -0500

    Add adoption status column to pets table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_21_044937) do
+ActiveRecord::Schema.define(version: 2020_08_23_005600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_08_21_044937) do
     t.bigint "shelter_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "adoption_status"
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
