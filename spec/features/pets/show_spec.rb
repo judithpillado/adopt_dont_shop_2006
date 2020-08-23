be7006c3f3e5e33264a56a0a8b2a4412a0475f65
Judith Pillado <judith.pillado@gmail.com>
2020-08-23 01:20:14 -0500

    Delete commented out User Story 9

--- spec/features/pets/show_spec.rb
+++ spec/features/pets/show_spec.rb
@@ -25,16 +25,3 @@ RSpec.describe "Pet Show Page" do
   end
 
 end
-
-
-# User Story 9, Pet Show
-#
-# As a visitor
-# When I visit '/pets/:id'
-# Then I see the pet with that id including the pet's:
-# - image
-# - name
-# - description
-# - approximate age
-# - sex
-# - adoptable/pending adoption status
